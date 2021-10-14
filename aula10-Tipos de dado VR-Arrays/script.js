68b6d769c703dadbff838799436436b83a8d6ba5
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 12:25:15 -0300

    aula10-Tipos de dados VR-Arrays

---
+++ aula10-Tipos de dado VR-Arrays/script.js
@@ -0,0 +1,13 @@
+var arr = [5, 'Dã', true, {teste: 1, teste:2,}];
+
+console.log(arr);
+
+console.log(arr[0]);
+//Para adicionarmos colocamos o número/vaga que não está ocupada e colocamos o valor, e fazemos o mesmo para mudar oq está dentro, indicando a vaga e botando outro valor//.
+
+arr[4] = 12;
+console.log(arr);
+
+
+
+
