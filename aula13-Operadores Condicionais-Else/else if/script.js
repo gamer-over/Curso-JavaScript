4550c4f4874a7bdb8730e4f1c3362bfd9694a3d9
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 19:42:27 -0300

    aula13-Operadores de Condição Else/else if if

---
+++ aula13-Operadores Condicionais-Else/else if/script.js
@@ -0,0 +1,21 @@
+var nome = 'Dã'
+
+if(nome == 'Dã'){
+    console.log('É o Dã');
+}else if(nome == 'Pedro'){
+console.log('É o Pedro');
+}else if(nome == 'Kauan'){
+console.log('É o Kauan');
+}else{
+    console.log('É outra pessoa');
+}
+
+
+var  idade = 19;
+
+if(idade > 20){
+    console.log('Pode entrar na festa');
+}else if(nome >= 18){
+    console.log('Só pode entrar com autorização');
+}
+
