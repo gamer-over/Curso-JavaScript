dc3cbde4e3acf0a7e92d6b792638fb5fc4d2bfe9
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 12:58:12 -0300

    aula11-Operador de comparação-if

---
+++ aula11-Operador condicional-if/script.js
@@ -0,0 +1,13 @@
+var idade = 16;
+var idademinima = 18;
+
+
+console.log("Antes do if");
+
+if(idade > idademinima){
+    console.log('Pode fazer a carteira de habilitação');
+}
+if(idade > 15){
+    console.log('Precisa espera mais 3 anos para tirar carta');
+}
+console.log('Depois do if');
