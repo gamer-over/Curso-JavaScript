b28b5705a8342c4ba7a79700fb29ec9f2b741729
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 11:53:15 -0300

    aula8-Tipos de dados VR-null e undefined

---
+++ aula8-Tipos de dado VR-null e undefined/script.js
@@ -0,0 +1,21 @@
+//Hoising-içamento
+
+console.log(sobrenome);
+console.log(numero);
+
+var nome =null;
+var sobrenome;
+
+console.log(nome);
+console.log(sobrenome);
+
+
+ nome = 'Dã';
+
+ var numero = 10;
+
+
+
+
+
+
