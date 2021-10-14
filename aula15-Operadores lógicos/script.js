d3177da5b4dcae76a316a7f1dfa4c0e4691a48bf
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 20:32:21 -0300

    aula15-Operador lógico &&

---
+++ aula15-Operadores lógicos/script.js
@@ -0,0 +1,15 @@
+//------&&--SO PASSA SE SATISFAZER TODAS AS APLICAÇÕES----//
+var idade = 16;
+var nome = 'Pedro';
+
+if(nome == 'Dã' && idade == 16){
+    console.log('O Dã pode entrar na aula de tiro');
+}else{
+    console.log('Não é o Dã');
+}
+
+if( 1 == 1 && 3 > 2 && true){
+    console.log('Passou');
+}
+//-------&&-----//
+
