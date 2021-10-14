eb419afc6c8d1cb7581b8ee9f025fc5337d5442c
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 11:24:07 -0300

    aula6-tipos de dados VR-String

---
+++ aula6-Tipos de dado VR-String/script.js
@@ -0,0 +1,14 @@
+var nome ='Dã';
+var sobrenome = 'Yaweh';
+
+var nomecompleto = nome +" "+ sobrenome;
+
+console.log(nomecompleto);
+
+console.log(nome);
+console.log(typeof nome);
+
+document.write('Olá');
+
+
+
