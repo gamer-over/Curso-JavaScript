a466afb9a140d1a17bc32c0a94353393e4b5fa12
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 10:53:46 -0300

    aula4-Variaveis

---
+++ aula4-variáveis/script.js
@@ -0,0 +1,25 @@
+var teste = 1;
+teste = 'Dã';
+
+console.log(teste);
+
+var $nome = 'Dã';
+var _nome = 'Biel';
+
+var meuPrimeiroNome = 'Antonio'
+console.log(meuPrimeiroNome);
+
+var meusobrenome = 'nidiani';
+console.log(meusobrenome);
+
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var nome;
+
+nome = 'olá';
+console.log(nome);
