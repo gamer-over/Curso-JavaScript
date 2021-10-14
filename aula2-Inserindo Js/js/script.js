56dc92d699647f793f9cb8b0f3d5174a57d61ec4
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 10:36:54 -0300

    Aulas 1,2,3

---
+++ aula2-Inserindo Js/js/script.js
@@ -0,0 +1,2 @@
+console.log("Script do Head");
+console.log("Script do Body");
---
+++ aula3-particularidadesJS/script.js
@@ -0,0 +1,14 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+var a = 1;
+a = 'teste';
+console.log(a);
+
+//Comentário em Js
+
+/* Este é
+um comentário de
+múltiplas linhas em
+JavaScript*/
