e1a492a3f1790f54b9aeccd0e499221ea8a63c68
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 19:17:57 -0300

    aula12-Operadores de comparação

---
+++ aula12-Operadores de comparação/script.js
@@ -0,0 +1,36 @@
+var idade = 18
+var temcarro = 0;
+
+if(idade >= 18){
+    console.log('Pode tirar carteira');
+}
+
+if(idade <= 17){
+    console.log('Não pode tirar a carteira');
+}
+
+if(temcarro){
+    console.log('Pode andar de carro');
+} 
+
+var nome = 'Dã'
+
+if(nome == 'Dã'){
+    console.log('Sou o Dã')
+}
+
+if(nome != 'Dã'){
+    console.log('Não sou o Dã');
+}
+
+if(20 > 100){
+    console.log('Passou 1');
+}
+
+if(100 < 20){
+    console.log('Passou 2')
+}
+
+
+
+//Não mostra nada pois o zero é considerado false e depois de 1 é considerado true//
