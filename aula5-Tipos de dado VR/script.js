966f48759a179aaec230aa0fc2064a0a9c6a4ede
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 11:10:26 -0300

    aula5-Variaveis-tipos de dados

---
+++ aula5-Tipos de dado VR/script.js
@@ -0,0 +1,16 @@
+var numero = 1;
+
+console.log(typeof numero);
+
+var float = 5.22;
+console.log(float);
+console.log(typeof float);
+
+var textocomnumero = '123';
+console.log(textocomnumero);
+console.log(typeof textocomnumero);
+
+var a = '123'
+var b = '456'
+console.log(a + b);
+
