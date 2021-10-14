4ba7e5fea3f24ceabb9f0d340f0e0c158e76303d
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-14 20:50:33 -0300

    aula17-Operadoreslógico-!

---
+++ aula17-Operadores lógicos 3/script.js
@@ -0,0 +1,14 @@
+//------!--MUDA O VALOR DA EXPRESSÃO----//
+if(!false){
+    console.log('Passou');
+}
+
+var nome = 'Jão';
+
+if(!nome == 'Dã'){
+    console.log('ok');
+}
+
+
+//-------!-----//
+
