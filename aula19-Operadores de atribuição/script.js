4e56fc0210c3e6dbf8d58dd1fb899e1ce26fd9ba
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-15 17:10:18 -0300

    aula19-Operadores de  atribuição

---
+++ aula19-Operadores de atribuição/script.js
@@ -0,0 +1,35 @@
+var x =1;
+var y = 2;
+
+//SOMA
+console.log(x = x + y);
+console.log(x += y);
+
+//SUBTRAÇÃO
+console.log(x -= y);
+
+//MULTIPLICAÇÃO
+console.log(x *= y);
+
+//DIVISÃO
+console.log(x /= y);
+
+//Loops
+console.log(x++);
+
+console.log(x--);
+
+while(x<= 100){
+  console.log(x);
+
+  x*= 2;
+}
+
+console.log(x);
+
+var j = 5;
+
+while(x > 0){
+  console.log(x);
+  x -= j;
+}
