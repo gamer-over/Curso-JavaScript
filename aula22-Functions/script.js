6b0b0e1cc62b0adfb1a78b400aab304a2c0770da
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-15 18:27:03 -0300

    aula22-Functions

---
+++ aula22-Functions/script.js
@@ -0,0 +1,33 @@
+function primeirafuncao(){
+
+console.log('Olá')
+
+}
+primeirafuncao();
+
+function dizernome(nome){
+
+console.log("O nome é " +nome);
+
+}
+dizernome("Dã");
+dizernome("ana");
+dizernome("biel");
+
+var nomebancodedados = 'Dã';
+dizernome(nomebancodedados);
+
+function soma(a,b){
+  var soma = a + b;
+  return soma;
+}
+
+var soma1 = soma(2,5);
+
+console.log(soma1);
+
+var soma2 = soma(2,7);
+
+console.log(soma2);
+
+console.log(soma(9,9));
