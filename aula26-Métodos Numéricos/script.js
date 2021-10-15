7ac07b06393ea18978e28acbf45fedd5962cc014
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-15 20:05:35 -0300

    aula26-Métodos Numéricos

---
+++ aula26-Métodos Numéricos/script.js
@@ -0,0 +1,30 @@
+//parseFloat-Números quebrados
+
+console.log(parseFloat('12.999'));
+console.log(Number.parseFloat(12.999));
+
+//parseInt-Número inteiro
+
+console.log(parseInt('16.999'));
+
+//toFixed-Vai limitar as casas decimais de um número
+
+console.log(23.999.toFixed(1));
+
+console.log(23.51551515.toFixed(1));
+
+//isNaN-Verifica o elemento para saber se é um número
+
+console.log(isNaN('teste'));
+console.log(isNaN(12));
+console.log(isNaN('15'));
+
+//Max_Vallue e Min_Vallue
+
+console.log(Number.MAX_VALLUE);
+console.log(Number.MIN_VALUE);
+
+console.log(23.51551515+300+300);
+
+
+
