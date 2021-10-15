a57a1d7cec66ee0be58c8b4bf6e88016850524da
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-15 16:48:22 -0300

    aula18-Sistema de repetição-While

---
+++ aula18-Sistema de repetição-While/script.js
@@ -0,0 +1,22 @@
+var x = 0;
+
+while(x <= 5){
+console.log('Testando repetição' + x);
+x++;
+}
+
+var arr = ['Teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <=3){
+  console.log(arr[y]);
+  y++;
+}
+
+var palavra = 'Dã';
+var i = 0;
+
+while(i < 8){
+  console.log(palavra[i]);
+  i+= 1;
+}
