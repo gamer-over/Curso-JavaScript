362accc456b498dc0f7f6c7eeac1acc7a8c87bea
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-15 19:42:55 -0300

    aula25-Let e Const

---
+++ aula25-Let e Const/script.js
@@ -0,0 +1,18 @@
+ let x = 5; //var x = 5;
+
+ const y = 10; /*Não podemos alterar o 
+                valor da variavel, por ela ser uma constante.
+                */
+ x = 12;
+
+ console.log(x);
+
+ if(true){
+
+let x = 20;
+
+console.log(x);
+
+}
+console.log(x);
+
