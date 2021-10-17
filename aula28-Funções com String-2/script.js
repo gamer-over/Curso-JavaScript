f44c905207b5853a10d5fccdacf065318c182995
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-17 13:28:35 -0300

    aula28-Funções com String-2

---
+++ aula28-Funções com String-2/script.js
@@ -0,0 +1,35 @@
+//toLowerCase-Letra minúscula e toUpperCase-Letra maiúscula
+
+var frase = 'Esta é a Frase'
+
+var frase = 'caixa alta';
+
+console.log(frase.toUpperCase());
+
+console.log(frase.toLowerCase());
+
+//trim-Trira todos os espaçoes feitos.
+
+var nome = '           Dã          ';
+
+var nometrin = nome.trim();
+
+console.log(nome);
+
+console.log(nometrin);
+
+//split-Transforma a string em array
+
+console.log(frase.split(" "));
+
+var tags = 'PHP , js, HTML, CSS';
+
+console.log(tags.split(" "));
+
+//lastIndexOf-Se houver duas palavras iguais,e quisermos a ultima devemos usar o lastIndexOf, para localizar a ultima.
+
+var frasedois = "Tenho um teste hoje e um teste amanha"
+
+console.log(frasedois.indexOf('teste'));
+
+console.log(frasedois.lastIndexOf('teste'));
