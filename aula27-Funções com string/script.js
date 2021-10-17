cce8c78b52de6d40400279006cf407843a87a456
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-17 12:47:36 -0300

    27-Funções com String

---
+++ aula27-Funções com string/script.js
@@ -0,0 +1,32 @@
+//length-ver quantidade de string da variavel
+
+var nome = 'Dã';
+
+console.log(nome.length);
+
+var obj = 'bola';
+console.log(obj.length);
+
+//indexof-decidir qual string quero ver
+
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de roma";
+
+console.log(frase.indexOf('roeu'));
+
+//slice-Remover alguma string da frase
+
+var roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+//replace-Trocar algumastring por outra da frase
+
+var novafrase = frase.replace('roeu', 'teste');
+
+console.log(novafrase);
+
+
+
+
