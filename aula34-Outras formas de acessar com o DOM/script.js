94a7b21ffb131770ef01f2179112005f59885fad
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-24 14:58:09 -0300

    QuerySelectorAll/querySelector

---
+++ aula34-Outras formas de acessar com o DOM/script.js
@@ -0,0 +1,31 @@
+var itensClasse = document.getElementsByClassName('item');
+
+console.log(itensClasse);
+
+var itensQuery = document.querySelectorAll('#list2 li');
+
+console.log(itensQuery);
+
+
+
+var itensQuery2 = document.querySelectorAll('#list .item');
+
+console.log(itensQuery2);
+
+//querySelector
+
+var lista = document.querySelector('#list');
+
+console.log(lista);
+
+var primeiralista = document.querySelector('ul');
+
+console.log(primeiralista);
+
+var span = document.querySelector('#paragrafo span');
+
+console.log(span);
+
+
+
+
