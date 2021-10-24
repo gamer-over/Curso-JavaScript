ae9354e864cad00b6ff6512a5e6b4d9427e98a75
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-24 11:41:29 -0300

    Acessando:tag,id e class com o DOM

---
+++ aula33-Acessando o DOM/script.js
@@ -0,0 +1,22 @@
+//tag
+
+var title = document.getElementsByTagName('h1')[0];
+
+console.log(title);
+
+var lis = document.getElementsByTagName('li')
+
+console.log(lis[3]);
+ 
+//id 
+
+var paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+//class
+
+var itemDalista = document.getElementsByClassName('item');
+
+console.log(itemDalista);
+
