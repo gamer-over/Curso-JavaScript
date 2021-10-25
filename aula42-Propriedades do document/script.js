de9610e9a985bd649d5cf80297570298eb617b0e
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-24 21:58:21 -0300

    Propriedades com document

---
+++ aula42-Propriedades do document/script.js
@@ -0,0 +1,17 @@
+//Propriedades documents
+
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links);
+
+document.links[0].textContent = "twitter";
+
+console.log(document.URL);
+
+console.log(document.title);
+
+document.title ="aula 42";
+
+console.log(document.title);
