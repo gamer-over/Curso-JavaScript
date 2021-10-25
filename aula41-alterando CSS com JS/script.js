c89c01425dc20c472e041464248f28437167dcd8
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-24 21:28:21 -0300

    Adicionando e alterando o CSS com o DOM

---
+++ aula41-alterando CSS com JS/script.js
@@ -0,0 +1,9 @@
+var title = document.querySelector('#title');
+
+title.style.color = "red";//podemos colocar core exadecimais também.
+
+title.style.backgroundColor = "orange";
+
+var subtitle = document.querySelector('#subtitle');
+
+subtitle.style.cssText = "color : blue;background-color: green; font-size : 70px; border-radius: 10px 10px";
