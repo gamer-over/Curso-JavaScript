0202d391f3cd4baf408f499c03295420569411c6
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-26 17:30:22 -0300

    Evento onload

---
+++ aula46-Evento onload/script.js
@@ -0,0 +1,16 @@
+// Carrega após o elemento do DOM//
+window.onload = function(){
+
+    console.log("Carregou o DOM");
+
+    var title2 = document.querySelector('#title');
+
+    console.log(title2);
+
+}
+
+console.log("Carregou o JS");
+
+var title = document.querySelector('#title');
+
+console.log(title);
