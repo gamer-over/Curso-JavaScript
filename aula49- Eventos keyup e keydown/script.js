67c87805aa644085aac323dc1cdf82a9f94fe37d
Dã Yaweh <90692286+gamer-over@users.noreply.github.com>
2021-10-26 18:25:10 -0300

    Eventos keyup e keydown

---
+++ aula49- Eventos keyup e keydown/script.js
@@ -0,0 +1,23 @@
+// key down //
+
+document.addEventListener("keydown", function(event){
+
+if(event.key ==="Enter"){
+
+console.log("Apertou enter");
+
+}
+
+});
+
+
+// keyup //
+
+document.addEventListener("keyup",function(event){
+
+    if(event.key ==="Enter"){
+    
+    console.log("Soltou enter");
+    
+    }
+    });
